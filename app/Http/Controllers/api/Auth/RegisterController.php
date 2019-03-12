d757caddcd344c6430c5d2c85dd96b68c3c4a6a6
rahman9868 <rahman9868@gmail.com>
2019-03-12 12:55:02 +0700

    #2 - Laravel + NuxtJS authentication: Registration and logging in

--- app/Http/Controllers/api/Auth/RegisterController.php
+++ app/Http/Controllers/api/Auth/RegisterController.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\api\Auth;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Http\Request;
+
 
 class RegisterController extends Controller
 {
@@ -28,18 +31,35 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '';
+    protected $auth;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(JWTAuth $auth)
     {
-        $this->middleware('guest');
+       $this->auth = $auth;
     }
 
+    public function register (Request $request){
+        $validator = $this->validator($request->all());
+        if (!$validator->fails()){
+            $user = $this->create($request->all());
+            $token = $this->auth->attempt($request->only('email','password'));
+            return response()->json([
+                'success' => true,
+                'data' => $user,
+                'token' => $token
+            ],200);
+        }
+        return response()->json([
+            'success' => false,
+            'errors' => $validator->errors()
+        ]);
+    }
     /**
      * Get a validator for an incoming registration request.
      *
@@ -49,9 +69,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello";
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
